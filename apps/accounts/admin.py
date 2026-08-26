0f137d82376f8fb0c0fc8ac7d5ee05bcfb1363f9
joshua <joshua.mbilli@student.moringaschool.com>
2026-08-26 12:17:17 +0300

    add configure account admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Invitation, Store, User
+
+admin.site.register(User)
+admin.site.register(Invitation)
+admin.site.register(Store)
